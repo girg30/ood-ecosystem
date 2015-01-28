13cc39acc6b5eab1a355cfc089edc809d9fb79f5
11130001@st.hcmuaf.edu.vn <11130001@st.hcmuaf.edu.vn@76c2c7bf-b230-45b9-2a7f-67f58ee71d87>
2015-01-28 19:47:17 +0000

    trước khi thay đổi List<Antelope> thành List<Object>

--- trunk/EcosystemSimulator/src/com/annvcit/model/ACarnivore.java
+++ trunk/EcosystemSimulator/src/com/annvcit/model/ACarnivore.java
@@ -1,17 +1,11 @@
 package com.annvcit.model;
 
-import java.awt.Rectangle;
 
 /**
  * lớp cha của các loài đv ăn thịt
  */
 public abstract class ACarnivore extends AAnimal {
-	
-	protected int radius = 500; // bán kính tìm mồi
-	
-	protected Rectangle body;
-	protected Rectangle radiusBound;
-	
+
 	public ACarnivore(int x, int y) { 
 		super(x, y); 
 	}
--- trunk/EcosystemSimulator/src/com/annvcit/model/Lion.java
+++ trunk/EcosystemSimulator/src/com/annvcit/model/Lion.java
@@ -4,34 +4,16 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Rectangle;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import com.annvcit.util.Util;
-import com.annvcit.util.Observable;
-import com.annvcit.util.Observer;
-
-import com.annvcit.model.Message;
-
 /**
  * Lion (Sư tử) là một loài động vật ăn thịt ở Africa
  */
-public class Lion extends ACarnivore implements Observable {
+public class Lion extends ACarnivore {
 
 	public Lion() {}
 	
 	public Lion(int x, int y) {
 		super(x, y);
 
-		this.w = 16;
-		this.h = 16;
-		
-		this.body = new Rectangle(x, y, w, h);
-		
-		// it's radius
-		int radiusX = body.x - (radius - body.width) / 2;
-		int radiusY = body.y - (radius - body.height) / 2;
-		radiusBound = new Rectangle(radiusX, radiusY, radius, radius);
 	}
 
 	public void draw(Graphics g) {
@@ -63,94 +45,4 @@ public class Lion extends ACarnivore implements Observable {
 
 	}
 
-	private List<Antelope> findVictim(List<Antelope> antelopeList) {
-
-		List<Antelope> victimList = new ArrayList<>();
-
-		for (Antelope a : antelopeList) {
-			if (this.radiusBound.intersects(a.getBody())) {
-				victimList.add(a);
-			}
-		}
-		return victimList;
-	}
-
-	private Antelope nearestVictim(List<Antelope> antelopeList) {
-		double distance = 0;
-		double minDistance = Integer.MAX_VALUE;
-		Antelope antelope = null;
-		for (int i = 0; i < antelopeList.size(); i++) {
-			distance = Util.distance(this.body, antelopeList.get(i).getBody());
-			if (distance < minDistance) {
-				minDistance = distance;
-				antelope = antelopeList.get(i);
-			}
-		}
-		return antelope;
-	}
-	
-	public void goHunt(List<Antelope> antelopeList) {
-		int step = 2;
-		List<Antelope> victimList = findVictim(antelopeList);
-		Antelope victim = nearestVictim(victimList);
-	
-		if (victim == null)
-			return;
-	
-		int victimX = victim.getBody().x;
-		int victimY = victim.getBody().y;
-	
-		if (victimX < body.x) {
-			this.body.x -= step;
-		}else if (victimX > body.x) {
-			this.body.x += step;
-		}
-	
-		if (victimY < body.y) {
-			this.body.y -= step;
-		}else if (victimY > body.y) {
-			this.body.y += step;
-		}
-	
-		if (body.intersects(victim.getBody())) {
-			Message messageHunt = new Message(Message.HUNT);
-			setChanged();
-			System.out.println("victim null: " + victim == null);
-			notifyObservers(messageHunt, this, victim);
-			
-		}
-	
-		setDelay(speed);
-	}
-	
-	//***************************************************
-	//				Observable						    *
-	//***************************************************
-	
-	private Observer africa;
-	private boolean isChanged = false;
-	
-	@Override
-	public void setChanged() {
-		isChanged = true;
-	}
-	
-	@Override
-	public void addObserver(Observer observer) {
-		africa = observer;
-	}
-	
-	@Override 
-	public void removeObserver(Observer observer) {
-		africa = null;
-	}
-	
-	@Override
-	public void notifyObservers(Object... objects) {
-		if (isChanged) {
-			africa.update(objects);
-		}
-	}
-	
-	
 }
