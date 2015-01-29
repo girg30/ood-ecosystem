c5ba899ba202fc101bccca32b7b9440f5a1ab04b
11130001 <11130001@st.hcmuaf.edu.vn>
2015-01-29 05:13:57 +0000

    KHÔNG LÀM ĐƯỢC, BUỒN VÃI

--- EcosystemSimulator/src/com/annvcit/model/ACarnivore.java
+++ EcosystemSimulator/src/com/annvcit/model/ACarnivore.java
@@ -1,6 +1,9 @@
 package com.annvcit.model;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import com.annvcit.util.Util;
 
 
 /**
@@ -10,11 +13,127 @@ public abstract class ACarnivore extends AAnimal {
 
 	public ACarnivore(int x, int y) { 
 		super(x, y); 
+		radius = 800;
 	}
 	
 	public ACarnivore() {}
 	
-	protected abstract void goHunt(List<AHerbivore> herbivoreList);
-    protected abstract List<AHerbivore> findVictim(List<AHerbivore> herbivoreList);
-    protected abstract AHerbivore nearestVictim(List<AHerbivore> victimList);
+	public List<AHerbivore> findVictim(List<AHerbivore> antelopeList) {
+
+		List<AHerbivore> victimList = new ArrayList<>();
+
+		for (AHerbivore a : antelopeList) {
+			if (this.radiusBound.intersects(a.getBody())) {
+				victimList.add(a);
+			}
+		}
+		return victimList;
+	}
+	
+	public List<ACarnivore> findCarnivore(List<ACarnivore> carnivoreList) {
+
+		List<ACarnivore> victimList = new ArrayList<>();
+
+		for (ACarnivore a : carnivoreList) {
+			if (this.radiusBound.intersects(a.getBody())) {
+				victimList.add(a);
+			}
+		}
+		return victimList;
+	}
+	
+	public ACarnivore nearestCarnivore(List<ACarnivore> carnivoreList) {
+		double distance = 0;
+		double minDistance = Integer.MAX_VALUE;
+		ACarnivore carnivore = null;
+		for (int i = 0; i < carnivoreList.size(); i++) {
+			distance = Util.distance(this.body, carnivoreList.get(i).getBody());
+			System.out.println(distance);
+			if (distance < minDistance) {
+				minDistance = distance;
+				carnivore = carnivoreList.get(i);
+			}
+		}
+		return carnivore;
+	}
+
+	public AHerbivore nearestVictim(List<AHerbivore> herbivoreList) {
+		double distance = 0;
+		double minDistance = Integer.MAX_VALUE;
+		AHerbivore antelope = null;
+		for (int i = 0; i < herbivoreList.size(); i++) {
+			distance = Util.distance(this.body, herbivoreList.get(i).getBody());
+			if (distance < minDistance) {
+				minDistance = distance;
+				antelope = herbivoreList.get(i);
+			}
+		}
+		return antelope;
+	}
+	
+
+	public void goHunt(List<AHerbivore> antelopeList) {
+		int step = 2;
+		List<AHerbivore> victimList = findVictim(antelopeList);
+		AHerbivore victim = nearestVictim(victimList);
+	
+		if (victim == null)
+			return;
+	
+		int victimX = victim.getBody().x;
+		int victimY = victim.getBody().y;
+	
+		if (victimX < body.x) {
+			this.body.x -= step;
+		}else if (victimX > body.x) {
+			this.body.x += step;
+		}
+	
+		if (victimY < body.y) {
+			this.body.y -= step;
+		}else if (victimY > body.y) {
+			this.body.y += step;
+		}
+	
+		if (body.intersects(victim.getBody())) {
+			Message messageHunt = new Message(Message.HUNT);
+			setChanged();
+			notifyObservers(messageHunt, this, victim);
+			
+		}
+	
+		setDelay(speed);
+	}
+	
+	public void goFight(List<ACarnivore> cannivoreList){ System.out.println("vo con");
+		int step = 2;
+		List<ACarnivore> victimList = findCarnivore(cannivoreList);
+		ACarnivore victim = nearestCarnivore(victimList);
+	
+		if (victim == null)
+			return;
+	
+		int victimX = victim.getBody().x;
+		int victimY = victim.getBody().y;
+	
+		if (victimX < body.x) {
+			this.body.x -= step;
+		}else if (victimX > body.x) {
+			this.body.x += step;
+		}
+	
+		if (victimY < body.y) {
+			this.body.y -= step;
+		}else if (victimY > body.y) {
+			this.body.y += step;
+		}
+	
+		if (body.intersects(victim.getBody())) {
+			Message messageHunt = new Message(Message.FIGHT_ME);
+			setChanged();
+			notifyObservers(messageHunt, this, victim);
+		}
+	
+		setDelay(speed);
+	}
 }
--- EcosystemSimulator/src/com/annvcit/model/Lion.java
+++ EcosystemSimulator/src/com/annvcit/model/Lion.java
@@ -3,10 +3,6 @@ package com.annvcit.model;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Rectangle;
-import java.util.ArrayList;
-import java.util.List;
-
-import com.annvcit.util.Util;
 
 /**
  * Lion (Sư tử) là một loài động vật ăn thịt ở Africa
@@ -25,18 +21,13 @@ public class Lion extends ACarnivore {
 		if (this.getCurrentState() instanceof ImplDeathState) {
 			g.setColor(Color.RED);
 			g.fillOval(body.x, body.y, body.width, body.height);
+			
 			if (power == DEAD_LINE*2) {
 				setChanged();
 				Message removeMeMessage = new Message(Message.REMOVE_ME);
 				notifyObservers(removeMeMessage, this);
 			}
 			return;
-		}
-		
-		if (power == 700) this.setCurrentState(this.getHungryState()); 
-		if (power < 0) {
-			this.setCurrentState(this.getDeathState());
-			
 		}
 
 		// lion
@@ -47,6 +38,28 @@ public class Lion extends ACarnivore {
 		
 		g.setColor(color);
 		g.fillOval(body.x, body.y, body.width, body.height);
+		
+		System.out.println(this.getCurrentState());
+		if (power == 700) this.setCurrentState(this.getHungryState()); 
+		if (power < 0) {
+			this.setCurrentState(this.getStarvedState());
+		}
+
+		if(this.getCurrentState() instanceof ImplStarvedState){
+			// TODO KHÔNG VÔ ĐÂY ĐƯỢC, GIỜ PHẢI LÀM SAO?????
+			int radiusX = body.x - (radius - body.width) / 2;
+			int radiusY = body.y - (radius - body.height) / 2;
+			radiusBound = new Rectangle(radiusX, radiusY, radius, radius);
+			
+			g.setColor(Color.RED);
+			g.drawOval(radiusBound.x, radiusBound.y, radiusBound.width,
+					radiusBound.height);
+			
+			if(power < DEAD_LINE + 400){
+				this.setCurrentState(this.getDeathState());
+			}
+			return;
+		}
 			
 		if (this.getCurrentState() instanceof ImplHungryState) {
 			// it's radius
@@ -62,64 +75,5 @@ public class Lion extends ACarnivore {
 
 	}
 	
-	public List<AHerbivore> findVictim(List<AHerbivore> antelopeList) {
-
-		List<AHerbivore> victimList = new ArrayList<>();
-
-		for (AHerbivore a : antelopeList) {
-			if (this.radiusBound.intersects(a.getBody())) {
-				victimList.add(a);
-			}
-		}
-		return victimList;
-	}
-
-	public AHerbivore nearestVictim(List<AHerbivore> antelopeList) {
-		double distance = 0;
-		double minDistance = Integer.MAX_VALUE;
-		AHerbivore antelope = null;
-		for (int i = 0; i < antelopeList.size(); i++) {
-			distance = Util.distance(this.body, antelopeList.get(i).getBody());
-			if (distance < minDistance) {
-				minDistance = distance;
-				antelope = antelopeList.get(i);
-			}
-		}
-		return antelope;
-	}
 	
-
-	public void goHunt(List<AHerbivore> antelopeList) {
-		int step = 2;
-		List<AHerbivore> victimList = findVictim(antelopeList);
-		AHerbivore victim = nearestVictim(victimList);
-	
-		if (victim == null)
-			return;
-	
-		int victimX = victim.getBody().x;
-		int victimY = victim.getBody().y;
-	
-		if (victimX < body.x) {
-			this.body.x -= step;
-		}else if (victimX > body.x) {
-			this.body.x += step;
-		}
-	
-		if (victimY < body.y) {
-			this.body.y -= step;
-		}else if (victimY > body.y) {
-			this.body.y += step;
-		}
-	
-		if (body.intersects(victim.getBody())) {
-			Message messageHunt = new Message(Message.HUNT);
-			setChanged();
-			notifyObservers(messageHunt, this, victim);
-			
-		}
-	
-		setDelay(speed);
-	}
-
 }
